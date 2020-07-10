2220e8f18739f2d7985cdeedf6ea476d7e214e18
Ariel Barclay <mrsbarclay101008@gmail.com>
2020-07-10 18:46:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(sentence)
   reversed_sent=[]
   words = sentence.split(" ")
  words.each do |word|
-   word.reverse
-   word.join
+   reversed_sent << word.reverse
+   
    
  end
  
